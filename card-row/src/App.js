6249fab1d6548cb7d8eba5f0c848101633e40f33
SANATH <kunjithayas@gmail.com>
2023-10-21 17:48:08 +0530

    first commit

---
+++ card-row/src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+
+import React from "react";
+import "./App.css";
+import CardRow from "./CardRow";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Card Row Example</h1>
+      <CardRow />
+    </div>
+  );
+}
+
+export default App;
+
